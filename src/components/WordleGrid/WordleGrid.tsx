33f111a46525c3944924831a17c0cb1b4de48afa
Henrike Stahlhut <henrike.st@icloud.com>
2024-08-30 22:13:44 +0200

    get radom word from json file

--- src/components/WordleGrid/WordleGrid.tsx
+++ src/components/WordleGrid/WordleGrid.tsx
@@ -1,7 +1,18 @@
+import React, { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 import GridRow from "./GridRow";
+import commonWords from "../../utils/common_five_letter_words.json";
 
 const WordleGrid = () => {
+  const [choosenWord, setChoosenWord] = useState("");
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * commonWords.length);
+    const randomWord = commonWords[randomIndex];
+    setChoosenWord(randomWord);
+  }, []);
+
+  console.log(choosenWord);
   return (
     <Grid container width={"50rem"} justifyContent={"center"}>
       {[...Array(5)].map((_, index) => (
