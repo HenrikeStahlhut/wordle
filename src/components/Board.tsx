d3a71ec4ac34e89d44ec1fb3684f4f372e7902e5
Henrike Stahlhut <henrike.st@icloud.com>
2024-10-19 18:12:43 +0200

    create Letter component, add Letter component to Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import { boardDefault } from "../utils/Board";
 import styled from "styled-components";
+import Letter from "./Letter";
 
 const Board = () => {
   const [board, setBoard] = useState(boardDefault);
 
   return (
     <BoardContainer>
-      <Row></Row>
-      <Row></Row>
-      <Row></Row>
-      <Row></Row>
-      <Row></Row>
-      <Row></Row>
+      {[...Array(5)].map((_, index) => (
+        <Row key={index}>
+          <Letter></Letter>
+          <Letter></Letter>
+          <Letter></Letter>
+          <Letter></Letter>
+          <Letter></Letter>
+        </Row>
+      ))}
     </BoardContainer>
   );
 };
---
+++ src/components/Letter.tsx
@@ -0,0 +1,10 @@
+interface LetterProps {
+  letterPosition: number;
+  attemptValue: string;
+}
+
+const Letter = () => {
+  return <div></div>;
+};
+
+export default Letter;
