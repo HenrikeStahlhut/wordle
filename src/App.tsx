8cf2190b4ca84aa166520cf01d0e96f977cf4b3b
Henrike Stahlhut <henrike.st@icloud.com>
2024-10-20 16:18:05 +0200

    add first logic to keys

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,33 @@ import { createContext } from "react";
 type BoardContextType = {
   board: string[][];
   setBoard: React.Dispatch<React.SetStateAction<string[][]>>;
+  currentAttempt: CurrentAttemptType;
+  setCurrentAttempt: React.Dispatch<React.SetStateAction<CurrentAttemptType>>;
+};
+
+type CurrentAttemptType = {
+  attempt: number;
+  letterPosition: number;
 };
 
 export const BoardContext = createContext<BoardContextType>({
   board: boardDefault,
   setBoard: () => {},
+  currentAttempt: { attempt: 0, letterPosition: 0 },
+  setCurrentAttempt: () => {},
 });
 
 function App() {
   const [board, setBoard] = useState<string[][]>(boardDefault);
+  const [currentAttempt, setCurrentAttempt] = useState<CurrentAttemptType>({
+    attempt: 0,
+    letterPosition: 0,
+  });
   return (
     <ThemeProvider theme={theme}>
-      <BoardContext.Provider value={{ board, setBoard }}>
+      <BoardContext.Provider
+        value={{ board, setBoard, currentAttempt, setCurrentAttempt }}
+      >
         <Layout />
       </BoardContext.Provider>
     </ThemeProvider>
--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import { BoardContext } from "../App";
 
 interface KeyProps {
   keyValue: any;
@@ -10,7 +12,24 @@ interface StyledKeyProps {
 }
 
 const Key = ({ keyValue, isBigKey }: KeyProps) => {
-  return <StyledKey isBigKey={isBigKey}>{keyValue}</StyledKey>;
+  const { board, setBoard, currentAttempt, setCurrentAttempt } =
+    useContext(BoardContext);
+
+  const selectLetter = () => {
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyValue;
+    setBoard(newBoard);
+    setCurrentAttempt({
+      ...currentAttempt,
+      letterPosition: currentAttempt.letterPosition + 1,
+    });
+  };
+
+  return (
+    <StyledKey isBigKey={isBigKey} onClick={selectLetter}>
+      {keyValue}
+    </StyledKey>
+  );
 };
 
 export default Key;
