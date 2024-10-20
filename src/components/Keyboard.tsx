025ff76536809a646ca064250d132a73310978a3
Henrike Stahlhut <henrike.st@icloud.com>
2024-10-20 15:20:31 +0200

    add first version of keyboard rows, add minor styling

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,5 +1,60 @@
+import styled from "styled-components";
+
 const Keyboard = () => {
-  return <>Keyboard</>;
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+  return (
+    <StyledKeyboard>
+      <KeyboardRow1>
+        {keys1.map((keys) => {
+          return <div>{keys}</div>;
+        })}
+      </KeyboardRow1>
+      <KeyboardRow2>
+        {keys2.map((keys) => {
+          return <div>{keys}</div>;
+        })}
+      </KeyboardRow2>
+      <KeyboardRow3>
+        {keys3.map((keys) => {
+          return <div>{keys}</div>;
+        })}
+      </KeyboardRow3>
+    </StyledKeyboard>
+  );
 };
 
 export default Keyboard;
+
+const StyledKeyboard = styled.div`
+  width: 700px;
+  height: 300px;
+  margin-top: 60px;
+`;
+
+const KeyboardRow1 = styled.div`
+  flex: 33%;
+  display: flex;
+  flex-direction: row;
+  display: flex;
+  justify-content: center;
+  margin: 5px;
+`;
+
+const KeyboardRow2 = styled.div`
+  flex: 33%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 5px;
+`;
+
+const KeyboardRow3 = styled.div`
+  flex: 33%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 5px;
+`;
