fcefaee7b75aa8dfba45107cad804b5eb5160c5f
Henrike Stahlhut <henrike.st@icloud.com>
2024-10-23 20:25:16 +0200

    add logic for using Enter and Backspace on physical keyboard

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,10 +1,39 @@
 import styled from "styled-components";
 import Key from "./Key";
+import { useCallback, useContext, useEffect } from "react";
+import { BoardContext } from "../App";
 
 const Keyboard = () => {
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+  const { handleSelectLetter, handleDeleteLetter, handleEnter } =
+    useContext(BoardContext);
+
+  const handleKeyboard = useCallback(
+    (e: any) => {
+      if (e.key === "Enter") {
+        handleEnter();
+      } else if (e.key === "Backspace") {
+        handleDeleteLetter();
+      } else {
+        [...keys1, ...keys2, ...keys3].forEach((key) => {
+          if (e.key === key) {
+            handleSelectLetter({ keyValue: key });
+          }
+        });
+      }
+    },
+    [handleEnter, handleDeleteLetter, handleSelectLetter],
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    };
+  }, [handleKeyboard]);
 
   return (
     <StyledKeyboard>
